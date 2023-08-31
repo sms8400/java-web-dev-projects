d7c2d0c12d297c8a6e1a5750191481d940f90b4a
Sue <138933788+sms8400@users.noreply.github.com>
2023-08-31 11:26:16 -0400

    done

--- classes-part-2/studio/src/main/java/org/launchcode/Main.java
+++ classes-part-2/studio/src/main/java/org/launchcode/Main.java
@@ -1,23 +1,23 @@
-//package org.launchcode;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Date;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        // write your code here
-//        System.out.println("Test");
-//        Menu myMenu = new Menu(new Date(),new ArrayList<MenuItem>(Arrays.asList(
-//                new MenuItem(4.75, "Chocolate shake", "Desserts", false),
-//                new MenuItem(5.50, "Chicken Tenders", "Chicken", false))));
-//        MenuItem myMenuItem = new MenuItem(6.50,"Bacon burger","Burgers", true);
-//        myMenu.addItem(myMenuItem);
-//        System.out.println(myMenu.toString());
-//        myMenu.addItem(myMenuItem);
-//        System.out.println(myMenuItem.toString());
-//        myMenu.removeItem(myMenuItem);
-//        System.out.println(myMenu.toString());
-//    }
-//}
+package org.launchcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Test");
+        Menu myMenu = new Menu(new Date(),new ArrayList<MenuItem>(Arrays.asList(
+                new MenuItem(4.75, "Chocolate shake", "Desserts", false),
+                new MenuItem(5.50, "Chicken Tenders", "Chicken", false))));
+        MenuItem myMenuItem = new MenuItem(6.50,"Bacon burger","Burgers", true);
+        myMenu.addItem(myMenuItem);
+        System.out.println(myMenu.toString());
+        myMenu.addItem(myMenuItem);
+        System.out.println(myMenuItem.toString());
+        myMenu.removeItem(myMenuItem);
+        System.out.println(myMenu.toString());
+    }
+}
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,6 +7,55 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void singleLeft() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void singleRight() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void simpleBalance() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void noBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test"));
+    }
+    @Test
+    public void wordEnclosed() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[test]"));
+    }
+    @Test
+    public void offsetBracketsLeft() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[test]phrase"));
+    }
+    @Test
+    public void offsetBracketsRight() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test[phrase]"));
+    }
+    @Test
+    public void multipleBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][[]]"));
+    }
+    @Test
+    public void multipleUnbalanced() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[[]]"));
+    }
+    @Test
+    public void newLineBalanced() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("\n[\n\n[\n]]\n"));
+    }
+    @Test
+    public void newLineUnbalanced() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("\n[\n\n[\n]\n"));
+    }
+
+    @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
