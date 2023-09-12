831a42587bc4e1618af709d9a05837bf8c06de00
Sue <138933788+sms8400@users.noreply.github.com>
2023-09-12 15:22:54 -0400

    Ch 11 ex

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
-
+        System.out.println(Divide(5,0));
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
@@ -13,15 +13,36 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        studentFiles.forEach((key, value) -> {
+            System.out.println(key + " - " + CheckFileExtension(value));
+        });
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static double Divide(int x, int y){
+        if ( y == 0 ) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return ((double) x/(double) y);
+        }
+        return 0;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName){
+        if ( fileName.isEmpty() ) {
+            try {
+                throw new RuntimeException("No file submitted");
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+                return -1;
+            }
+        } else if (fileName.endsWith(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
